ed2a8a7397f26596a2a0d61647046858ff85a5c5
chrisbolosan <cbolosan@gmail.com>
2022-01-30 15:44:56 -0600

    track schema

---
+++ server/src/schema.js
@@ -0,0 +1,22 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    tracksForHome: [Track!]!
+  }
+
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+`;
+module.exports = typeDefs;
