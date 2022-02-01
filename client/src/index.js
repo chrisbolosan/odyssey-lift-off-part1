92091e895f554a790e9f6b42b70c68d5b756021e
chrisbolosan <cbolosan@gmail.com>
2022-01-31 18:24:43 -0600

    apollo client wrapper up

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <GlobalStyles />
+      <Pages />
+    </React.StrictMode>
+  </ApolloProvider>,
+
+  document.getElementById("root")
 );
